e0379a2a0b8f637ca4e43bfb4050c0940aec9291
wilkwo <2381773226@qq.com>
2022-09-01 21:29:31 +0800

    引入element-plus

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import ElementPlus from "element-plus"
+import 'element-plus/dist/index.css'
 
 import './assets/main.css'
 
 const app = createApp(App)
 
+app.use(ElementPlus)
 app.use(createPinia())
 app.use(router)
 
