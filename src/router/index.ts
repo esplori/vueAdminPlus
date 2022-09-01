500b5f8534663215b5d91af29afa2f74eacf2a6c
wilkwo <2381773226@qq.com>
2022-09-01 22:34:16 +0800

    新增登录页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,7 +9,13 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/HomeView.vue')
+      component: () => import('../views/HomeView.vue'),
+      children:[
+        {
+          path: 'login',
+          component: () => import('../views/account/login.vue'),
+        }
+      ]
     }
   ]
 })
