134e9ea48daa1582fb655a6a91597adc4a3741b2
wilkwo <2381773226@qq.com>
2022-09-03 14:05:05 +0800

    新增样式文件

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import router from './router'
 import ElementPlus from "element-plus"
 import 'element-plus/dist/index.css'
 
-import './assets/main.css'
+import './assets/styles/main.css'
 
 const app = createApp(App)
 
