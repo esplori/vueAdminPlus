2baed2e48dd4c7f1af8b99cd54b7a49e02c5b066
wilkwo <2381773226@qq.com>
2022-09-03 21:46:05 +0800

    更新引入文件

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from "element-plus"
-import 'element-plus/dist/index.css'
-import './assets/styles/index.css'
+import './assets/styles/index.scss'
 
 const app = createApp(App)
 
