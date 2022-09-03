51a2de6e0545a84662bfd7501f702c1a75c2bca0
wilkwo <2381773226@qq.com>
2022-09-03 16:36:31 +0800

    更新css

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from "element-plus"
 import 'element-plus/dist/index.css'
-
-import './assets/styles/main.css'
+import './assets/styles/index.css'
 
 const app = createApp(App)
 
