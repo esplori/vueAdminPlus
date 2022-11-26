9914abd2ecd8bce811f635ad7cfd2f979f6147d7
2EN <2381773226@QQ.COM>
2022-11-26 11:24:44 +0800

    更新登录页面

--- src/views/account/reg.vue
+++ src/views/account/reg.vue
@@ -144,25 +144,19 @@ const valid = async () => {
       margin-top: 0;
       background: rgba(255, 255, 255, 1);
       padding: 30px 60px;
-      // box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-      border-radius: 49% 51% 46% 54% / 52% 49% 51% 48%   ;
-      box-shadow: inset 10px 20px 30px rgba(0,0,0, 0.2), 
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+      border-radius: 100% 0% 100% 0% / 0% 100% 0% 100%   ;
+      box-shadow: inset 1px 1px 1px rgba(0,0,0, 0.2), 
                   10px 10px 20px rgba(0, 0, 0, 0.1), 
-                  15px 15px 30px rgba(0, 0, 0, 0.05),
+                  1px 1px 1px rgba(0, 0, 0, 0.05),
                   inset -10px -10px 15px rgba(255,255,255, 0.8);
       animation: action 3s linear infinite alternate;
       @keyframes action {
-        25%{
-          border-radius: 43% 57% 40% 60% / 53% 44% 56% 47%     ;
-        }
         50%{
-          border-radius: 38% 62% 35% 65% / 56% 39% 61% 44%       ;
-        }
-        75%{
-          border-radius: 58% 42% 51% 49% / 35% 57% 43% 65%       ;
+          border-radius:  95% 5% 97% 3% / 4% 96% 4% 96%          ;
         }
         100%{
-          border-radius: 64% 36% 59% 41% / 30% 65% 35% 70%      ;
+          border-radius:  98% 2% 97% 3% / 2% 96% 4% 98%     ;
         }
         
       }
