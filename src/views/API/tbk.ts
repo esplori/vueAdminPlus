f05ceace4b914364ba7897e36af46cc01a276395
2EN <2381773226@QQ.COM>
2022-12-18 12:51:20 +0800

    update interface url

--- src/views/API/tbk.ts
+++ src/views/API/tbk.ts
@@ -1,41 +1,41 @@
 import { get, post } from "@/utils/axios.js";
-const contextPath = "/tbk-service";
+const contextPath = "/promote-service";
 
 export async function getTbkShopListApi(data: any) {
-  const url = `${contextPath}/tbk/getList`;
+  const url = `${contextPath}/promote/getList`;
   const options = {};
   return get(url, data, options);
 }
 export async function getTbkListApi(data: any) {
-  const url = `${contextPath}/tbk/getList/${data.page}`;
+  const url = `${contextPath}/promote/getList/${data.page}`;
   const options = {};
   return get(url, {}, options);
 }
 
 export async function getCateApi(data: any) {
-  const url = `${contextPath}/tbk/getCate`;
+  const url = `${contextPath}/promote/getCate`;
   const options = {};
   return get(url, data, options);
 }
 
 export async function insertCateApi(data: Object) {
-  const url = `${contextPath}/tbk/insertCate`;
+  const url = `${contextPath}/promote/insertCate`;
   const options = { showLoading: true };
   return post(url, data, options);
 }
 
 export async function updateCateApi(data: Object) {
-  const url = `${contextPath}/tbk/updateCate`;
+  const url = `${contextPath}/promote/updateCate`;
   const options = { showLoading: true };
   return post(url, data, options);
 }
 export async function delCateApi(data: Object) {
-  const Url = `${contextPath}/tbk/delCate`;
+  const Url = `${contextPath}/promote/delCate`;
   const options = { showLoading: true };
   return post(Url, data, options);
 }
 export async function delTbkApi(data: Object) {
-  const Url = `${contextPath}/tbk/delItem`;
+  const Url = `${contextPath}/promote/delItem`;
   const options = { showLoading: true };
   return post(Url, data, options);
 }
