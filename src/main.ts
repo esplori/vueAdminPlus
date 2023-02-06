94647180eecbca32598bd46df0e966726c1937ee
2EN <2381773226@QQ.COM>
2023-02-06 22:41:53 +0800

    按需加载elementPlust组件 打包优化

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-import ElementPlus from "element-plus";
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
+// import ElementPlus from "element-plus";
+// import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import "./assets/styles/index.scss";
 
 const app = createApp(App);
-app.use(ElementPlus,{size:"default",locale: zhCn});
+// app.use(ElementPlus,{size:"default",locale: zhCn});
 app.use(createPinia());
 app.use(router);
 
