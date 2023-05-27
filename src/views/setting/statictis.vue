3ea723b79175043d571f2308585353da7096cc8b
wilkwo <2381773226@qq.com>
2023-05-28 00:15:57 +0800

    更新echart为完整引入

--- src/views/setting/statictis.vue
+++ src/views/setting/statictis.vue
@@ -164,41 +164,41 @@ import { getWebStatisticsApi } from "@/views/API/stats.js";
 import { CountUp } from "countup.js";
 
 // 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
-import * as echarts from 'echarts/core';
+import * as echarts from 'echarts';
 // 引入柱状图图表，图表后缀都为 Chart
-import { PieChart, LineChart } from 'echarts/charts';
+// import { PieChart, LineChart } from 'echarts/charts';
 // 引入提示框，标题，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
-import {
-  TitleComponent,
-  TooltipComponent,
-  GridComponent,
-  DatasetComponent,
-  TransformComponent,
-  LegendComponent
-} from 'echarts/components';
+// import {
+//   TitleComponent,
+//   TooltipComponent,
+//   GridComponent,
+//   DatasetComponent,
+//   TransformComponent,
+//   LegendComponent
+// } from 'echarts/components';
 // 标签自动布局、全局过渡动画等特性
-import { LabelLayout, UniversalTransition } from 'echarts/features';
+// import { LabelLayout, UniversalTransition } from 'echarts/features';
 // 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
-import { CanvasRenderer } from 'echarts/renderers';
+// import { CanvasRenderer } from 'echarts/renderers';
 
 import { reactive, computed, onMounted, nextTick } from "vue";
 import { CaretTop, CaretBottom } from "@element-plus/icons-vue";
 import { delHtmlTag } from "@/utils/common.js";
 import { userInfoStore } from '@/stores/userInfo'
 // 注册必须的组件
-echarts.use([
-  TitleComponent,
-  TooltipComponent,
-  GridComponent,
-  DatasetComponent,
-  TransformComponent,
-  PieChart,
-  LineChart,
-  LabelLayout,
-  UniversalTransition,
-  CanvasRenderer,
-  LegendComponent
-]);
+// echarts.use([
+//   TitleComponent,
+//   TooltipComponent,
+//   GridComponent,
+//   DatasetComponent,
+//   TransformComponent,
+//   PieChart,
+//   LineChart,
+//   LabelLayout,
+//   UniversalTransition,
+//   CanvasRenderer,
+//   LegendComponent
+// ]);
 const state = reactive({
   views: 0,
   pages: 0,
@@ -416,8 +416,8 @@ const initDayViews = () => {
   });
   myChart.resize();
 };
-const getWebStatistics = async (type:string) => {
-  const res: any = await getWebStatisticsApi({type: type});
+const getWebStatistics = async (type: string) => {
+  const res: any = await getWebStatisticsApi({ type: type });
   if (res) {
     state.views = res.data.allViews;
     state.pages = res.data.allpages;
@@ -452,17 +452,18 @@ const initCharts = () => {
     initDayViews();
   });
 };
-let tabChange =(type:string)=> {
+let tabChange = (type: string) => {
   getWebStatistics(type);
 }
 </script>
 
 <style scoped lang="scss">
 .home {
-  .dailySentence{
+  .dailySentence {
     border: 1px dashed #d8d0d0;
     margin-bottom: 20px;
   }
+
   .date-picker-change {
     padding: 20px 0;
     display: flex;
