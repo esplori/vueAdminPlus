147c585aec659dce7019caf49e7fcf7745ded341
wilkwo <2381773226@qq.com>
2023-10-11 23:23:07 +0800

    更新图标标签

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -174,7 +174,7 @@ export function dayViewsOptions(state: any) {
     },
     series: [
       {
-        name: "今日访问IP数(UV)",
+        name: "独立访客数(UV)",
         type: "line",
         stack: "Total",
         areaStyle: {
@@ -199,7 +199,7 @@ export function dayViewsOptions(state: any) {
         showSymbol: false
       },
       {
-        name: "今日浏览量(PV)",
+        name: "访问量(PV)",
         type: "line",
         stack: "Total",
         showSymbol: false,
