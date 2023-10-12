e75f3277f11f1832ddc038cc783ceaee9e17d270
wilkwo <2381773226@qq.com>
2023-10-12 22:09:05 +0800

    新增埋点

--- src/views/components/footer.vue
+++ src/views/components/footer.vue
@@ -10,6 +10,22 @@
 import { ref } from "vue";
 const copyRight = ref("&copy");
 const currentDate = ref(new Date().getFullYear());
+const initWebStat = () => {
+  let webStats = new webStatistics({
+    baseUrl: "/manage-service", // 基础接口地址url
+    url: "/stats/getStats.gif", // 请求上报api的接口地址
+    routeMode: "history", // 填写单页面应用中使用的路由模式。
+    autoUpload: true, // 是否自动请求接口，在setUserId之后会以baseUrl+url形式在页面切换时自动请求上报PV/UV的接口
+    prop: {
+      //请求参数映射，参数名默认如下，可以自定义修改参数名。
+      id: "visitorId",
+    },
+  });
+  setTimeout(() => {
+    webStats.setUserId();
+  }, 1000);
+}
+initWebStat()
 </script>
 <style lang="scss" scoped>
 .footer {
