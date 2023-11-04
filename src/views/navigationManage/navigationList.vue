0fe8c068da64f1ed39919d07a5012b533f0fa901
wilkwo <2381773226@qq.com>
2023-11-04 23:08:02 +0800

    更新样式

--- src/views/navigationManage/navigationList.vue
+++ src/views/navigationManage/navigationList.vue
@@ -15,7 +15,7 @@
     </searchHeader>
     <el-table :data="state.list" style="width: 100%">
       <!-- <el-table-column type="index" width="55" label="序号"> </el-table-column> -->
-      <el-table-column prop="title" label="标题"> </el-table-column>
+      <el-table-column prop="title" label="标题" show-overflow-tooltip> </el-table-column>
       <el-table-column prop="content" label="描述" show-overflow-tooltip>
       </el-table-column>
       <el-table-column prop="url" label="地址" show-overflow-tooltip>
@@ -26,7 +26,7 @@
       <el-table-column prop="cateName" label="分类"> </el-table-column>
       <el-table-column prop="createDate" label="创建日期" show-overflow-tooltip>
       </el-table-column>
-      <el-table-column label="操作" width="180">
+      <el-table-column label="操作" >
         <template #default="scope">
           <el-button link @click="edit(scope.row)" type="primary">编辑</el-button>
           <el-button link @click="delConfirm(scope.row.id)" type="danger">删除</el-button>
