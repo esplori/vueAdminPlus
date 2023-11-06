47fc2e9cb513819891aca62102da518dec3469b3
wilkwo <2381773226@qq.com>
2023-11-06 23:47:47 +0800

    更新定时任务列表

--- src/views/setting/scheduleList.vue
+++ src/views/setting/scheduleList.vue
@@ -3,19 +3,19 @@
     <searchHeader :title="'定时任务'"></searchHeader>
     <el-table :data="state.list">
       <el-table-column label="任务ID" prop="id" width="100px"></el-table-column>
-      <el-table-column label="任务名称" width="100px">
+      <el-table-column label="任务名称" >
         <template #default="scope">
           <span v-if="!scope.row.isEdit">{{ scope.row.cron_name }}</span>
           <el-input v-else v-model="scope.row.cron_name"></el-input>
         </template>
       </el-table-column>
-      <el-table-column label="通知时间">
+      <el-table-column label="定时任务（cron）表达式">
         <template #default="scope">
           <span v-if="!scope.row.isEdit">{{ scope.row.cron_date }}</span>
           <el-input v-else v-model="scope.row.cron_date"></el-input>
         </template>
       </el-table-column>
-      <el-table-column label="操作">
+      <el-table-column label="操作" width="100px">
         <template #default="scope">
           <el-button type="primary" v-if="!scope.row.isEdit" link @click="edit(scope.row)">编辑</el-button>
           <el-button type="primary" v-else link @click="save(scope.row)">保存</el-button>
