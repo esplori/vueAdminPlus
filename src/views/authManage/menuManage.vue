5cd5958d6462043ba8d46d1fc9655929c0516ca1
wilkwo <2381773226@qq.com>
2023-12-25 21:10:10 +0800

    更新菜单添加交互为弹窗

--- src/views/authManage/menuManage.vue
+++ src/views/authManage/menuManage.vue
@@ -5,6 +5,19 @@
       <el-col :span="12">
         <el-tree node-key="menuId" default-expand-all :data="state.data" :expand-on-click-node="false"
           :props="state.defaultProps" @node-click="handleNodeClick">
+          <template #default="{ node, data }">
+            <span class="custom-tree-node">
+              <span>{{ node.label }}</span>
+              <span class="add-delete">
+                <el-icon @click="state.showAdd = true">
+                  <Plus />
+                </el-icon>
+                <el-icon @click="deleteMenuConfirm()">
+                  <Delete />
+                </el-icon>
+              </span>
+            </span>
+          </template>
         </el-tree>
       </el-col>
       <el-col :span="12">
@@ -16,38 +29,34 @@
             <el-input v-model="state.form.path"></el-input>
           </el-form-item>
           <el-form-item label="">
-            <el-button type="primary" @click="state.showAdd = true">新增</el-button>
             <el-button type="primary" @click="updateMenu(state.form)">保存</el-button>
-            <el-button type="danger" @click="deleteMenuConfirm()">删除</el-button>
           </el-form-item>
         </el-form>
-        <div v-if="state.showAdd">
-          <div style="
-              text-align: center;
-              padding: 10px;
-              border-bottom: 1px solid #ddd;
-              margin-bottom: 10px;
-            ">
-            新增菜单
-          </div>
-          <el-form :model="state.addForm">
-            <el-form-item label="菜单名称">
-              <el-input v-model="state.addForm.name"></el-input>
-            </el-form-item>
-            <el-form-item label="菜单地址">
-              <el-input v-model="state.addForm.path"></el-input>
-            </el-form-item>
-            <el-form-item label="">
-              <el-button type="primary" @click="append(state.addForm)">保存</el-button>
-            </el-form-item>
-          </el-form>
-        </div>
       </el-col>
     </el-row>
+    <el-dialog title="新增菜单" v-model="state.showAdd" width="30%">
+      <div>
+        <el-form :model="state.addForm">
+          <el-form-item label="菜单名称">
+            <el-input v-model="state.addForm.name"></el-input>
+          </el-form-item>
+          <el-form-item label="菜单地址">
+            <el-input v-model="state.addForm.path"></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="state.showAdd = false">取 消</el-button>
+          <el-button type="primary" @click="append(state.addForm)">确 定</el-button>
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { Plus, Delete } from "@element-plus/icons-vue";
 import {
   insertMenuApi,
   getAllMenusApi,
@@ -109,6 +118,7 @@ const append = async (form: any) => {
   }
   insertMenuApi({ ...parms }).then(() => {
     ElMessage.success("新增成功");
+    state.showAdd = false;
     getMenuList();
   });
 };
@@ -134,3 +144,17 @@ const deleteMenu = (form: any) => {
   });
 };
 </script>
+<style lang="scss" scoped>
+.custom-tree-node {
+  display: flex;
+  align-items: center;
+
+  .add-delete {
+    margin-left: 8px;
+
+    i {
+      margin-left: 10px;
+    }
+  }
+}
+</style>
