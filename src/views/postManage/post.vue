1f62fa1a280530f64f006703464ee31a1d6441b8
wilkwo <2381773226@qq.com>
2023-12-27 22:54:07 +0800

    编辑功能新增操作历史记录

--- src/views/postManage/post.vue
+++ src/views/postManage/post.vue
@@ -1,12 +1,7 @@
 <template>
   <div class="_post">
     <div id="editor-toolbar">
-      <Toolbar
-        style="z-index: 100"
-        :editor="editorRef"
-        :defaultConfig="toolbarConfig"
-        :mode="mode"
-      />
+      <Toolbar style="z-index: 100" :editor="editorRef" :defaultConfig="toolbarConfig" :mode="mode" />
     </div>
     <div id="content">
       <div id="editor-container">
@@ -14,39 +9,24 @@
           <input v-model="state.form.title" placeholder="请输入标题..." />
         </div>
         <div id="editor-text-area">
-          <Editor
-            style="height: 500px; overflow-y: hidden"
-            v-model="valueHtml"
-            :defaultConfig="editorConfig"
-            :mode="mode"
-            @onCreated="handleCreated"
-            @onChange="handleChange"
-          />
+          <Editor style="height: 500px; overflow-y: hidden" v-model="valueHtml" :defaultConfig="editorConfig" :mode="mode"
+            @onCreated="handleCreated" @onChange="handleChange" />
+          <div class="draft-btn" @click="handleDraft">
+            <!-- <el-button :icon="Edit"></el-button> -->
+            <span>草稿</span>
+            <span>记录</span>
+          </div>
         </div>
         <!-- 文章选项 -->
         <div class="post-info">
           <el-form label-width="85px" :model="state.form">
             <el-form-item label="创建时间：">
-              <el-date-picker
-                type="datetime"
-                style="width: 250px"
-                :disabled-date="disabledDate"
-                popper-class="select-zindex"
-                v-model="state.form.createDate"
-              ></el-date-picker>
+              <el-date-picker type="datetime" style="width: 250px" :disabled-date="disabledDate"
+                popper-class="select-zindex" v-model="state.form.createDate"></el-date-picker>
             </el-form-item>
             <el-form-item label="分类：">
-              <el-select
-                v-model="state.form.cate"
-                popper-class="select-zindex"
-                class="optionsWidth"
-              >
-                <el-option
-                  v-for="(item, index) in state.cateList"
-                  :key="index"
-                  :label="item.name"
-                  :value="item.id"
-                >
+              <el-select v-model="state.form.cate" popper-class="select-zindex" class="optionsWidth">
+                <el-option v-for="(item, index) in state.cateList" :key="index" :label="item.name" :value="item.id">
                 </el-option>
               </el-select>
             </el-form-item>
@@ -58,27 +38,14 @@
               ></el-input>
             </el-form-item> -->
             <el-form-item label="关键字：">
-              <el-tag
-                :key="tag"
-                v-for="tag in state.dynamicTags"
-                closable
-                :disable-transitions="false"
-                @close="handleClose(tag)"
-              >
+              <el-tag :key="tag" v-for="tag in state.dynamicTags" closable :disable-transitions="false"
+                @close="handleClose(tag)">
                 {{ tag }}
               </el-tag>
-              <el-input
-                class="input-new-tag"
-                v-if="state.inputVisible"
-                v-model="state.inputValue"
-                ref="saveTagInput"
-                @keyup.enter="handleInputConfirm"
-                @blur="handleInputConfirm"
-              >
+              <el-input class="input-new-tag" v-if="state.inputVisible" v-model="state.inputValue" ref="saveTagInput"
+                @keyup.enter="handleInputConfirm" @blur="handleInputConfirm">
               </el-input>
-              <el-button v-else class="button-new-tag" @click="showInput"
-                >+ 新增</el-button
-              >
+              <el-button v-else class="button-new-tag" @click="showInput">+ 新增</el-button>
             </el-form-item>
           </el-form>
         </div>
@@ -93,25 +60,40 @@
             </el-icon>
             <span>草稿保存中...</span>
           </span>
-          <span v-else>草稿已于 {{getCurrDate("")}} 自动保存成功</span>
+          <span v-else>草稿已于 {{ getCurrDate("") }} 自动保存成功</span>
         </div>
         <div>
           <span class="wordNum">文章字数：{{ state.form.wordsNum }}</span>
         </div>
         <div>
-          <el-button
-            :loading="state.postLoading"
-            @click="submit(false)"
-            type="primary"
-            >发布</el-button
-          >
+          <el-button :loading="state.postLoading" @click="submit(false)" type="primary">发布</el-button>
         </div>
       </div>
     </div>
+    <el-dialog :title="state.draft.title" v-model="state.draft.dialogVisible" width="30%">
+      <div>
+        <el-table :data="state.draft.list" style="width: 100%" height="500">
+          <el-table-column label="序号" type="index" width="80px"> </el-table-column>
+          <el-table-column label="标题" show-overflow-tooltip>
+            <template #default="scope">
+              {{ scope.row.title }}
+            </template>
+          </el-table-column>
+          <el-table-column label="创建时间" prop="createDate"> </el-table-column>
+          <el-table-column label="操作" width="180">
+            <template #default="scope">
+              <a :href="'/#/post?from=draft&did=' + scope.row.id + '&dindex=' + scope.row.dateStamp" target="_blank"
+                rel="noopener noreferrer">查看</a>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script lang="ts" setup>
 import "@wangeditor/editor/dist/css/style.css"; // 引入 css
+import { Edit } from "@element-plus/icons-vue";
 import {
   onBeforeUnmount,
   ref,
@@ -132,20 +114,18 @@ import {
   getAdminCateValidApi,
 } from "@/views/API/admin.js";
 
-onMounted(() => {
-  autoSave();
-});
-// 组件销毁时，也及时销毁编辑器
-onBeforeUnmount(() => {
-  window.clearInterval(state.autoSaveInterval);
-  const editor = editorRef.value;
-  if (editor) {
-    editor.destroy();
-  }
-});
+
 
 const router = useRouter();
 const route = useRoute();
+const id = route.query.id;
+const did = route.query.did as string;
+const dindex = route.query.dindex as string;
+const mode = "default";
+// 编辑器实例，必须用 shallowRef
+const editorRef = shallowRef();
+// 内容 HTML
+const valueHtml = ref("");
 
 const state = reactive({
   form: {
@@ -158,6 +138,7 @@ const state = reactive({
     createBy: "",
     wordsNum: 0,
     createDate: getCurrDate(""),
+    dateStamp: ""
   },
   cateList: [{ name: "", id: "" }],
   inputVisible: false,
@@ -166,9 +147,38 @@ const state = reactive({
   autoSaveInterval: 0,
   loading: null,
   postLoading: false,
+  draft: {
+    title: "查看历史记录",
+    dialogVisible: false,
+    list: [],
+  }
 });
+onMounted(() => {
+  if (id) {
+    getDetail(id);
+  } else if (did && dindex) {
+    // 从草稿过来时从本地缓存获取对应内容
+    getDraftFromLocalstorage(did, dindex);
+    setTimeout(() => {
+      // 加延时，等编辑器加载完成在渲染
+      getDraftFromLocalstorage(did, dindex);
+    }, 500)
+  }
+  getCate(id);
+  if (!did) {
+    // 从草稿过来的内容不开启定时保存
+    autoSave();
+  }
 
-const id = route.query.id;
+});
+// 组件销毁时，也及时销毁编辑器
+onBeforeUnmount(() => {
+  window.clearInterval(state.autoSaveInterval);
+  const editor = editorRef.value;
+  if (editor) {
+    editor.destroy();
+  }
+});
 
 const userInfo = computed(() => {
   const uinfo = localStorage.getItem("userInfo");
@@ -179,7 +189,7 @@ const userInfo = computed(() => {
   }
 });
 
-const disabledDate = (date: any)=>{
+const disabledDate = (date: any) => {
   //  今天之后不能选择
   return date > new Date();
 }
@@ -187,7 +197,7 @@ const disabledDate = (date: any)=>{
 const getCate = async (id: any) => {
   const res: any = await getAdminCateValidApi({
     pageNum: 1,
-    pageSize:100,
+    pageSize: 100,
   });
   if (res) {
     state.cateList = res.data.result || [];
@@ -210,11 +220,22 @@ const getDetail = async (id: any) => {
     editorRef.value.setHtml(state.form.content);
   }
 };
+const getDraftFromLocalstorage = (did: string, dindex: string) => {
+  let tempPostList = localStorage.getItem("tempPostList") as any;
+  if (tempPostList) {
+    tempPostList = JSON.parse(tempPostList)
+    let indexList = tempPostList[did].filter((item: any) => {
+      return item.dateStamp == dindex
+    })
+    // 找到当前草稿
+    if (indexList.length) {
+      let currPost = indexList[0]
+      state.form = currPost
+      editorRef.value.setHtml(state.form.content);
+    }
 
-if (id) {
-  getDetail(id);
+  }
 }
-getCate(id);
 
 const editPage = async (isDraft: boolean) => {
   if (isDraft) {
@@ -262,11 +283,6 @@ const postPage = async (isDraft: boolean) => {
   }
 };
 
-const mode = "default";
-// 编辑器实例，必须用 shallowRef
-const editorRef = shallowRef();
-// 内容 HTML
-const valueHtml = ref("");
 
 const toolbarConfig = {
   excludeKeys: [
@@ -307,6 +323,27 @@ editorConfig.MENU_CONF["uploadImage"] = {
     insertFn(res.data[0].url);
   },
 };
+const saveToLocalStorage = () => {
+  // 手动添加时间戳
+  state.form.createDate = getCurrDate(new Date());
+  state.form.dateStamp = String(new Date().getTime());
+  let tempPostList = localStorage.getItem("tempPostList") as any;
+  if (tempPostList) {
+    tempPostList = JSON.parse(tempPostList)
+    // 自动保存同时保存到缓存，通过内容id跟时间戳作为唯一id
+    if (state.form.id) {
+      if (!tempPostList[state.form.id]) {
+        tempPostList[state.form.id] = []
+      }
+      tempPostList[state.form.id].unshift(state.form);
+      // 最新保存数据排最上面
+      state.draft.list = JSON.parse(JSON.stringify(tempPostList[state.form.id]))
+    }
+  } else {
+    tempPostList = {}
+  }
+  localStorage.setItem("tempPostList", JSON.stringify(tempPostList))
+}
 
 const submit = async (type: boolean) => {
   state.form.content = editorRef.value.getHtml() as any;
@@ -323,8 +360,8 @@ const submit = async (type: boolean) => {
     ElMessage.warning("请选择分类");
     return false;
   }
-  // 同时保存到缓存
-  localStorage.setItem("postInfo", JSON.stringify(state.form));
+  // 自动保存记录
+  saveToLocalStorage();
   if (state.form.id) {
     editPage(type);
   } else {
@@ -355,8 +392,11 @@ const autoSave = () => {
       submit(true);
     }
     console.log(state.autoSaveInterval);
-  }, 15000);
+  }, 20000);
 };
+const handleDraft = () => {
+  state.draft.dialogVisible = true;
+}
 </script>
 
 <style scoped lang="scss">
@@ -402,6 +442,7 @@ const autoSave = () => {
   }
 
   #editor-text-area {
+    position: relative;
     min-height: 500px;
     margin-top: 20px;
   }
@@ -454,6 +495,29 @@ const autoSave = () => {
   .el-tag {
     margin-right: 5px;
     height: 32px;
+  }
+
+  .draft-btn {
+    position: absolute;
+    left: -110px;
+    top: 30%;
+    display: flex;
+    flex-direction: column;
+    border: 1px solid #8590a6;
+    background-color: var(--el-color-primary);
+    color: #fff;
+    padding: 10px;
+    cursor: pointer;
+    border-radius: 4px;
+  }
+
+  .draft-item {
+    text-align: left;
+    padding: 5px 10px;
+
+    &.hover {
+      text-decoration: underline;
+    }
   }
 }
 </style>
